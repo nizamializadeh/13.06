822a103d86f642506afbc1b1ef86b4b614978d54
Nizami <nizamializade0@gmail.com>
2017-07-06 13:53:12 +0400

    Add files via upload

---
+++ test13/script.js
@@ -0,0 +1,9 @@
+var obj = document.createElement("div");
+document.body.appendChild(obj);
+obj.style.width="1500px";
+obj.style.height="1000px";
+obj.style.background="lightgray";
+obj.style.margin="0 auto";
+
+
+
