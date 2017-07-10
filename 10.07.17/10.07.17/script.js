1201848147211bcf2ecfd13d750b4fe92d73f2a6
Nizami <nizamializade0@gmail.com>
2017-07-11 00:43:17 +0400

    Add files via upload

---
+++ 10.07.17/10.07.17/script.js
@@ -0,0 +1,23 @@
+var clientWitdh = document.querySelector(".item").clientWidth;
+iteration=0;
+function left(){
+	iteration+=clientWitdh;
+	if (iteration<-9000) {
+		iteration=0;
+	}
+	document.querySelector(".imgslide").style.left=iteration+"px";
+	console.log(iteration)
+}
+function right(){
+	iteration-=clientWitdh;
+	if (iteration>0) {
+		iteration=-8060;
+	}
+	document.querySelector(".imgslide").style.left=iteration+"px";
+	console.log(iteration)
+}
+var a = document.getElementsByTagName("button")[+0];
+var img = document.getElementsByTagName("img")[0];
+a.addEventListener("click",function () {
+	img.style.opacity = "0";
+})
