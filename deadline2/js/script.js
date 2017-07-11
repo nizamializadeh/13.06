9c6e351784bd756e888a2f557b00b8fd9b406838
Nizami <nizamializade0@gmail.com>
2017-07-11 16:44:58 +0400

    Add files via upload

---
+++ deadline2/js/script.js
@@ -0,0 +1,34 @@
+
+
+
+// clientwitdh = document.querySelector(".item").clientwitdh;
+// iteration=0;
+// function right() {
+// 	iteration-=clientwitdh;
+// 	if (iteration<-3200) {
+// 		iteration=0;
+// 	}
+// 	document.querySelector(".imgslide").style.left=iteration+"px";
+// 	console.log(iteration)
+// }
+
+
+// clientwitdh = document.querySelector(".wrapper").clientwitdh;
+// iteration=0;
+	
+function right1() {
+	var a = document.getElementsByClassName("nese1")[0];
+		a.style.marginLeft = "0px";
+}
+function left1() {
+	var a = document.getElementsByClassName("nese1")[0];
+		a.style.marginLeft = "-1300px";
+}
+function right() {
+	var a = document.getElementsByClassName("nese")[0];
+		a.style.marginLeft = "0px";
+}
+function left() {
+	var a = document.getElementsByClassName("nese")[0];
+		a.style.marginLeft = "-689px";
+}
