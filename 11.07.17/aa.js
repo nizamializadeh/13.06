962648c63709dca9c84ab9a95b5168311bc3611e
Nizami <nizamializade0@gmail.com>
2017-07-14 03:31:44 +0400

    Add files via upload

--- 11.07.17/aa.js
+++ 11.07.17/aa.js
@@ -3,26 +3,31 @@
 // 	img.style.marginLeft = "-200px";
 // 	console.log(this)
 // })
+// var az = document.getElementsByTagName("img")[0];
 var aqa= ["img/image1.jpg","img/image2.jpg","img/image3.jpg","img/image4.jpg",];
 for (i=0;i<aqa.length;i++){
+	
 var a = document.createElement("div");
 document.body.appendChild(a);
 var b = document.createElement("img")
 a.appendChild(b);
 b.setAttribute("src", aqa[i])
-b.addEventListener("click",function(){
-	
-	this.style.zIndex = [9]
-	console.log(b)
-	console.log(b.style.zIndex = [i+9]);
-	console.log(this)
-	b.style.position = 'relative';
 
-	b.style.transform = 'translateX(20px)'
 
-})
-	
+
 }
 
+	var name = document.querySelector("img")
+	
+	name.addEventListener("click",function(){
+		
+	name.style.zIndex = [5];
+	name.style.position = 'relative';
+	name.style.transform = 'translateX(-20px)'
 
 
+	console.log(this);
+	console.log(name)
+	
+
+})
