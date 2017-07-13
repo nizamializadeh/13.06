bc0c57c1e6cfd96d94d298de138a1e62319f5140
Nizami <nizamializade0@gmail.com>
2017-07-14 00:04:11 +0400

    Add files via upload

--- 11.07.17/aa.js
+++ 11.07.17/aa.js
@@ -1,7 +1,28 @@
-var img = document.querySelectorAll("img")
+// var img = document.getElementsByClassName("item")[0]
+// img.addEventListener("click",function(){
+// 	img.style.marginLeft = "-200px";
+// 	console.log(this)
+// })
+var aqa= ["img/image1.jpg","img/image2.jpg","img/image3.jpg","img/image4.jpg",];
+for (i=0;i<aqa.length;i++){
+var a = document.createElement("div");
+document.body.appendChild(a);
+var b = document.createElement("img")
+a.appendChild(b);
+b.setAttribute("src", aqa[i])
+b.addEventListener("click",function(){
+	
+	this.style.zIndex = [9]
+	console.log(b)
+	console.log(b.style.zIndex = [i+9]);
+	console.log(this)
+	b.style.position = 'relative';
 
-for(i=0; i<img.leght;i++){
-	img[i].addEventListener("click",function(){
-	this.style.marginLeft = "200px";
+	b.style.transform = 'translateX(20px)'
+
 })
+	
 }
+
+
+
